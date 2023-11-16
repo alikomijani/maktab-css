55855cd175d76bc9eba6eca2537bb8d9b0145d71
Ali <a.komijani@wallex.net>
2023-11-16 10:01:53 +0330

    manual form validation

--- src/components/widget/user-form/UserForm.jsx
+++ src/components/widget/user-form/UserForm.jsx
@@ -1,30 +1,41 @@
 import React, { useEffect, useState } from "react";
 import { Button } from "../../base";
-
+import "./user-form.style.css";
+import { validateUserForm } from "./validation";
+import clsx from "clsx";
 const emptyUser = {
   username: "",
   name: "",
   email: "",
+  age: 0,
 };
 export function UserForm({ initialValue = emptyUser, onSubmit, ...props }) {
   const [user, setUser] = useState(initialValue || emptyUser);
+  const [errors, setErrors] = useState({});
   const [pending, setPending] = useState(false);
+  console.log({ errors });
   const handleSubmit = (e) => {
     e.preventDefault();
-    setPending(true);
-    onSubmit(user)
-      .then(() => {
-        alert("success");
-        setUser(emptyUser);
-      })
-      .catch((error) => {
-        if (error) {
-          console.log(error.data);
-        }
-      })
-      .finally(() => {
-        setPending(false);
-      });
+    // setPending(true);
+    const { hasError, fromErrors } = validateUserForm(user);
+    console.log({ hasError, fromErrors });
+    if (hasError) {
+      setErrors(fromErrors);
+      return;
+    }
+    // onSubmit(user)
+    //   .then(() => {
+    //     alert("success");
+    //     setUser(emptyUser);
+    //   })
+    //   .catch((error) => {
+    //     if (error) {
+    //       console.log(error.data);
+    //     }
+    //   })
+    //   .finally(() => {
+    //     setPending(false);
+    //   });
   };
   const handleInput = (e) => {
     setUser((old) => ({ ...old, [e.target.name]: e.target.value }));
@@ -38,19 +49,21 @@ export function UserForm({ initialValue = emptyUser, onSubmit, ...props }) {
   }, [initialValue]);
   return (
     <form onSubmit={handleSubmit} {...props}>
-      <div>
-        <label>
+      <div className="form-control">
+        <label htmlFor="username_id">
           <span>username</span>
-          <input
-            type="text"
-            name="username"
-            required
-            value={user.username}
-            onChange={handleInput}
-          />
         </label>
+        <input
+          type="text"
+          name="username"
+          value={user.username}
+          onChange={handleInput}
+          required
+          className={clsx(errors.username && "input-error")}
+        />
+        {errors.username ? <span className="">{errors.username}</span> : null}
       </div>
-      <div>
+      <div className="form-control">
         <label>
           <span>name</span>
           <input
@@ -61,7 +74,20 @@ export function UserForm({ initialValue = emptyUser, onSubmit, ...props }) {
           />
         </label>
       </div>
-      <div>
+      <div className="form-control">
+        <label>
+          <span>age</span>
+          <input
+            type="number"
+            name="age"
+            max={18}
+            min={10}
+            value={user.age}
+            onChange={handleInput}
+          />
+        </label>
+      </div>
+      <div className="form-control">
         <label>
           <span>email</span>
           <input
@@ -72,7 +98,7 @@ export function UserForm({ initialValue = emptyUser, onSubmit, ...props }) {
           />
         </label>
       </div>
-      <div>
+      <div className="form-control">
         <Button disabled={pending} type="submit">
           Save
         </Button>
