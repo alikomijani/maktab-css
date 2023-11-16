537105468e576a5720788eca3c62e140e5362f40
Ali <a.komijani@wallex.net>
2023-11-16 12:35:09 +0330

    add formik hook

--- src/components/widget/formik-user-form/FormikUserForm.jsx
+++ src/components/widget/formik-user-form/FormikUserForm.jsx
@@ -4,6 +4,30 @@ import "./user-form.style.css";
 import { validateUserForm } from "./manual-validation";
 import clsx from "clsx";
 import { convertErrors, userSchema } from "./yup-validation";
+
+import { useFormik } from "formik";
+const formFields = [
+  {
+    name: "username",
+    label: "Username",
+    type: "text",
+  },
+  {
+    name: "name",
+    label: "Name",
+    type: "text",
+  },
+  {
+    name: "email",
+    label: "Email",
+    type: "email",
+  },
+  {
+    name: "age",
+    label: "Age",
+    type: "number",
+  },
+];
 const emptyUser = {
   username: "",
   name: "",
@@ -15,112 +39,97 @@ export function FormikUserForm({
   onSubmit,
   ...props
 }) {
-  const [user, setUser] = useState(initialValue || emptyUser);
-  const [errors, setErrors] = useState({});
-  const [pending, setPending] = useState(false);
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    // setPending(true);
-    //==================start manual validation===================//
-    // const { hasError, fromErrors } = validateUserForm(user);
-    // if (hasError) {
-    //   setErrors(fromErrors);
-    //   return;
-    // }
-    //==================end manual validation===================//
-    //==================start yup validation===================//
-    try {
-      console.log(user);
-      const value = await userSchema.validate(user, {
-        abortEarly: false,
-      });
-      console.log(value);
-      // start submit form
-    } catch (err) {
-      setErrors(convertErrors(err.inner));
-    }
+  const {
+    errors,
+    handleBlur,
+    setValues,
+    handleChange,
+    values,
+    isSubmitting,
+    setSubmitting,
+    setErrors,
+    handleSubmit,
+  } = useFormik({
+    initialValues: initialValue ?? emptyUser,
+    onSubmit: (data) => {
+      setSubmitting(true);
+      setTimeout(() => {
+        onSubmit(data)
+          .then((res) => console.log(res))
+          .catch((error) => {
+            console.log(error);
+            // you can use setError to show backend errors
+          })
+          .finally(() => {
+            setSubmitting(false);
+          });
+      }, 2000);
+    },
+    validateOnBlur: true,
+    validateOnChange: false,
+    validate: (values) => {
+      const { fromErrors } = validateUserForm(values);
+      return fromErrors;
+    },
+    validationSchema: userSchema,
+  });
 
-    //==================end yup validation===================//
-    // start form submit
-    // onSubmit(user)
-    //   .then(() => {
-    //     alert("success");
-    //     setUser(emptyUser);
-    //   })
-    //   .catch((error) => {
-    //     if (error) {
-    //       console.log(error.data);
-    //     }
-    //   })
-    //   .finally(() => {
-    //     setPending(false);
-    //   });
-  };
-  const handleInput = (e) => {
-    setUser((old) => ({ ...old, [e.target.name]: e.target.value }));
-  };
   useEffect(() => {
     if (initialValue) {
-      setUser(initialValue);
+      setValues(initialValue);
     } else {
-      setUser(emptyUser);
+      setValues(emptyUser);
     }
-  }, [initialValue]);
+  }, [initialValue, setValues]);
   return (
     <form onSubmit={handleSubmit} {...props}>
-      <div className="form-control">
-        <label htmlFor="username_id">
-          <span>username</span>
-        </label>
-        <input
-          type="text"
-          name="username"
-          value={user.username}
-          onChange={handleInput}
-          required
-          className={clsx(errors.username && "input-error")}
+      {formFields.map((field) => (
+        <FormControl
+          key={field.name}
+          error={errors[field.name]}
+          value={values[field.name]}
+          handleChange={handleChange}
+          handleBlur={handleBlur}
+          name={field.name}
+          label={field.label}
+          type={field.type}
         />
-        {errors.username ? <span className="">{errors.username}</span> : null}
-      </div>
+      ))}
       <div className="form-control">
-        <label>
-          <span>name</span>
-          <input
-            type="text"
-            name="name"
-            value={user.name}
-            onChange={handleInput}
-          />
-        </label>
-        {errors.name ? <span className="">{errors.name}</span> : null}
-      </div>
-      <div className="form-control">
-        <label>
-          <span>age</span>
-          <input
-            type="number"
-            name="age"
-            value={user.age}
-            onChange={handleInput}
-          />
-        </label>
-        {errors.age ? <span className="">{errors.age}</span> : null}
-      </div>
-      <div className="form-control">
-        <label>
-          <span>email</span>
-          <input
-            type="text"
-            name="email"
-            value={user.email}
-            onChange={handleInput}
-          />
-        </label>
-        {errors.email ? <span className="">{errors.email}</span> : null}
-      </div>
-      <div className="form-control">
-        <Button type="submit">Save</Button>
+        <Button
+          className={clsx(isSubmitting && "loading")}
+          disabled={isSubmitting}
+          type="submit"
+        >
+          {isSubmitting ? "...Saving" : "Save"}
+        </Button>
       </div>
     </form>
   );
 }
+
+const FormControl = ({
+  label,
+  name,
+  handleChange,
+  value,
+  error,
+  handleBlur,
+  type = "text",
+}) => {
+  return (
+    <div className="form-control">
+      <label>
+        <span>{label}</span>
+        <input
+          type={type}
+          onBlur={handleBlur}
+          name={name}
+          value={value}
+          onChange={handleChange}
+        />
+      </label>
+      {error ? <span className="">{error}</span> : null}
+    </div>
+  );
+};
